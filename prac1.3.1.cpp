b142fc18009ee71e1abcc45f0c2cd04a859aeb84
bobchin <bobchin.ryu@gmail.com>
2016-07-15 09:13:11 +0900

    練習問題1.3.1を追加

---
+++ prac1.3.1.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    double time, jikyu;
+    cout << "]‹Æˆõ‚Ì˜J“­ŽžŠÔ‚ð“ü—Í‚µ‚Ä‚­‚¾‚³‚¢:";
+    cin >> time;
+
+    cout << "]‹Æˆõ‚ÌŽž‹‹‚ð“ü—Í‚µ‚Ä‚­‚¾‚³‚¢:";
+    cin >> jikyu;
+
+    cout << "‡Œv’À‹à‚Í" << time * jikyu << "‚Å‚·B";
+
+    return 0;
+}
+
+
