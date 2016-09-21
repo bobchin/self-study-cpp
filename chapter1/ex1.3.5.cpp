2b1d6f431939987a47008963126045b1a42e0eba
kazunobu <k-ichihashi@hokkai-v.co.jp>
2016-09-21 13:50:53 +0900

    ex1.3.5.を追加

---
+++ chapter1/ex1.3.5.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    char ch;
+
+    cout << "キーを入力。xで停止\n";
+    do
+    {
+        cout << "： ";
+        cin >> ch;
+    } while(ch != 'x');
+    
+    return 0;
+}
