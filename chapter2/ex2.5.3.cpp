cbaa7d72f639916d64f976e8aa3e84f51eca6839
kazunobu <k-ichihashi@hokkai-v.co.jp>
2016-09-26 17:13:21 +0900

    ex2.5.1を追加 構造体のコンストラクタとデストラクタ

---
+++ chapter2/ex2.5.3.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <cstring>
+#include <cstdlib>
+using namespace std;
+
+// 構造体のコンストラクタとデストラクタ
+
+struct strtype
+{
+    strtype(const char *ptr);
+    ~strtype();
+    void show();
+private:
+    char *p;
+    int len;
+};
+
+strtype::strtype(const char *ptr)
+{
+    len = strlen(ptr);
+    p = (char *)malloc(len + 1);
+    if (!p) {
+        cout << "メモリ割り当てエラー\n";
+        exit(1);
+    }
+    strcpy(p, ptr);
+}
+
+strtype::~strtype()
+{
+    cout << "pを解放する\n";
+    free(p);
+}
+
+void strtype::show()
+{
+    cout << p << " - 長さ: " << len << "\n";
+}
+
+int main() {
+    strtype s1("This is a test."), s2("I like C++.");
+
+    s1.show();
+    s2.show();
+
+    return 0;
+}
+
+
+
+
+
