fbb4f8f5e58d518052f890dd650efd7826c900e4
kazunobu <k-ichihashi@hokkai-v.co.jp>
2016-09-26 18:17:38 +0900

    ex2.7.1を追加

---
+++ chapter2/ex2.7.1.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+class samp {
+    int i, j;
+public:
+    samp(int a, int b) { i = a; j = b; }
+    int divisible() { return !(i % j); }
+};
+
+int main() {
+    samp ob1(10, 2), ob2(10, 3);
+
+    if (ob1.divisible()) {
+        cout << "10は2で割り切れる\n";
+    } else {
+        cout << "10は2で割り切れない\n";
+    }
+    if (ob2.divisible()) {
+        cout << "10は3で割り切れる\n";
+    } else {
+        cout << "10は3で割り切れない\n";
+    }
+
+    return 0;
+}
