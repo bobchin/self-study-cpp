8e76aad98b631853f92cb17e9675842120c6a127
kazunobu <k-ichihashi@hokkai-v.co.jp>
2016-10-03 16:36:23 +0900

    ex3.3.1を追加 オブジェクトを返すサンプル

---
+++ chapter3/ex3.3.1.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+class samp
+{
+    char s[80];
+public:
+    void show() { cout << s << "\n"; }
+    void set(char *str) { strcpy(s, str); }
+};
+
+//　samp型のオブジェクトを返す
+samp input()
+{
+    char s[80];
+    samp str;
+
+    cout << "文字列の入力:";
+    cin >> s;
+
+    str.set(s);
+
+    return str;
+}
+
+int main()
+{
+    samp ob;
+
+    ob = input();
+    ob.show();
+
+    return 0;
+}
+
+
