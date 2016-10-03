db8621ebe9195b34166e3de8c4c868b44c1f588a
kazunobu <k-ichihashi@hokkai-v.co.jp>
2016-10-03 15:00:14 +0900

    ex3.1.2を追加 クラスのオブジェクトを代入すると値がコピーされる

---
+++ chapter3/ex3.1.2.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+using namespace std;
+
+#define SIZE 10
+
+class stack
+{
+    char stck[SIZE];
+    int tos;
+public:
+    stack();
+    void push(char ch);
+    char pop();
+};
+
+stack::stack()
+{
+    cout << "スタックを生成する\n";
+    tos = 0;
+}
+
+void stack::push(char ch)
+{
+    if (tos == SIZE) {
+        cout << "スタックは一杯です\n";
+        return;
+    }
+    stck[tos] = ch;
+    tos++;
+}
+
+char stack::pop()
+{
+    if (tos == 0) {
+        cout << "スタックは空です\n";
+        return 0;
+    }
+    tos--;
+    return stck[tos];
+}
+
+int main() {
+    stack s1, s2;
+    int i;
+
+    s1.push('a');
+    s1.push('b');
+    s1.push('c');
+
+    s2 = s1;
+
+    for (i = 0; i < 3; i++) {
+        cout << "s1をポップする:" << s1.pop() << "\n";
+    }
+    for (i = 0; i < 3; i++) {
+        cout << "s2をポップする:" << s2.pop() << "\n";
+    }
+
+    return 0;
+}
+
+
+
+
