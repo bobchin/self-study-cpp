12f6badd150059013b7f87ad3b08c37b7274c7a4
kazunobu <k-ichihashi@hokkai-v.co.jp>
2016-10-03 15:18:08 +0900

    ex3.2.2を追加 関数は値渡しなので、引数のオブジェクトはコピーされて渡される

---
+++ chapter3/ex3.2.2.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+class samp
+{
+    int i;
+public:
+    samp(int n) { i = n; }
+    void set_i(int n) { i = n; }
+    int get_i() { return i; }
+};
+
+void sqr_it(samp o)
+{
+    o.set_i(o.get_i() * o.get_i());
+    cout << "iのコピーの値は " << o.get_i() << "\n";
+}
+
+int main()
+{
+    samp a(10);
+    sqr_it(a);
+
+    cout << "しかし、a.i は main() で変更されない: ";
+    cout << a.get_i() << "\n";
+    
+    return 0;
+}
+
