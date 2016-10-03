8ef78d2817dcad6d3952eef028cf406445cb96cf
kazunobu <k-ichihashi@hokkai-v.co.jp>
2016-10-03 11:11:24 +0900

    check2.3を追加

---
+++ chapter2/check2.3.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int i = 10;
+    long l = 1000000;
+    double d = -0.009;
+
+    cout << i << ' ' << l << ' ' << d << "\n";
+
+    return 0;
+}
