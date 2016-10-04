bcdd1bba1022cb63ed9898215293d36c519a8a98
kazunobu <k-ichihashi@hokkai-v.co.jp>
2016-10-04 18:17:14 +0900

    prac4.2.1を追加

---
+++ chapter4/prac4.2.1.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+class samp
+{
+    int a, b;
+public:
+    samp(int n, int m) { a = n; b = m; }
+    int get_a() { return a; }
+    int get_b() { return b; }
+};
+
+int main()
+{
+    samp ob[4] = {
+        samp(1, 2),
+        samp(3, 4),
+        samp(5, 6),
+        samp(7, 8)
+    };
+
+    int i;
+    samp *p;
+    p = &ob[3];
+
+    for (i = 0; i < 4; i++)
+    {
+        cout << p->get_a() << " " << p->get_b() << "\n";
+        p--;
+    }
+
+
+    return 0;
+}
