e3e4297ba7130494c41e8b9699648792076a5a09
kazunobu <k-ichihashi@hokkai-v.co.jp>
2016-10-05 13:38:43 +0900

    ex4.6.1を追加 参照を使って２つの整数を入れ替える

---
+++ chapter4/ex4.6.1a.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+// 参照ではなくポインタの場合
+void swapargs(int *x, int *y);
+
+int main()
+{
+    int i, j;
+
+    i = 10;
+    j = 19;
+
+    cout << "変換前: ";
+    cout << "i:" << i << ", ";
+    cout << "j:" << j << "\n";
+
+    swapargs(&i, &j);
+
+    cout << "変換後: ";
+    cout << "i:" << i << ", ";
+    cout << "j:" << j << "\n";
+
+    return 0;
+}
+
+void swapargs(int *x, int *y)
+{
+    int t;
+    t = *x;
+    *x = *y;
+    *y = t;
+}
