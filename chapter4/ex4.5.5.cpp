ca213c97e10f4dc0d8b1a0cf6a3b08931ed20b4f
kazunobu <k-ichihashi@hokkai-v.co.jp>
2016-10-05 13:18:57 +0900

    ex4.5.5を追加 オブジェクトの動的配列の解放時にデストラクタがコールされる

---
+++ chapter4/ex4.5.5.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+class samp
+{
+    int i, j;
+public:
+    void set_ij(int a, int b) { i = a; j = b; }
+    int get_product() { return i * j; }
+    ~samp() { cout << "デストラクタ呼び出し\n"; }
+};
+
+int main()
+{
+    samp *p;
+    int i;
+
+    p = new samp[10];
+    if (!p) {
+        cout << "メモリ割り当てエラー\n";
+        return 1;
+    }
+
+    for (i = 0; i < 10; i++)
+    {
+        p[i].set_ij(i, i);
+    }
+    for (i = 0; i < 10; i++)
+    {
+        cout << "積[" << i << "]は: " << p[i].get_product() << "\n";
+    }
+
+    delete [] p;
+
+    return 0;
+}
